df3bb9583e69c55a1ece77c91aa72ff84e7dd0fa
AndyDAntuono <andrea.dantuono88@gmail.com>
2024-09-24 16:33:46 +0200

    [UPD] inserted comic.php; created layouts directory and app.blade.php

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+<!-- copio ed incollo l'header originale dalla repo vite comics, ma per il momento terrò commentata qusta parte di codice per evitare disatri
+<header>
+        <div class="container">
+            <div class="row">
+                <div class="col-12">
+                    <div class="d-flex justify-content-between align-intems-center py-3 bg-white">
+                        <img src="../assets/img/dc-logo.png" />
+                        <nav class="navbar navbar-expand-lg">
+                            <div class="container-fluid">
+                                <div class="collapse navbar-collapse">
+                                    <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                                        <li v-for="link, index in links" :key="index" :class="['nav-item', link.current ? 'active' : '']">
+                                          <a class="nav-link" :href="link.url">{{ link.label }}</a>
+                                        </li>
+                                      </ul>
+                                </div>
+                            </div>
+                        </nav>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </header>
+-->
+</body>
+</html>
