9a49c37788f8e468a1a65af72241f791d25bba43
AndyDAntuono <andrea.dantuono88@gmail.com>
2024-09-24 21:35:52 +0200

    [UPD] updated products.blade.php, web.php, db.php and README.md files

--- resources/views/products.blade.php
+++ resources/views/products.blade.php
@@ -1,4 +1,33 @@
 @extends('layouts.app')
 
 @section('content')
+<main>
+    <div class="container">
+        <div class="row">
+            <div class="col-12">
+                <div class="jumbotron">
+                </div>
+            </div>
+        </div>
+        <div class="row py-3">
+            @foreach ($comics as $comic)
+            <div class="col-2">
+                <div class="card mb-4">
+                    <img src="{{ $comic['thumb'] }}" class="card-img-top" alt="{{ $comic['title'] }}">
+                    <div class="card-body">
+                        <h5 class="card-title">{{ $comic['title'] }}</h5>
+                        <p class="card-text">{{ $comic['description'] }}</p>
+                        <ul class="list-group list-group-flush">
+                            <li class="list-group-item"><strong>Prezzo:</strong> {{ $comic['price'] }}</li>
+                            <li class="list-group-item"><strong>Serie:</strong> {{ $comic['series'] }}</li>
+                            <li class="list-group-item"><strong>Data di uscita:</strong> {{ $comic['sale_date'] }}</li>
+                            <li class="list-group-item"><strong>Tipo:</strong> {{ $comic['type'] }}</li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+            @endforeach
+        </div>
+    </div>
+</main>
 @endsection
--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,9 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('home');
 });
+
+Route::get('/', function (){
+    $comics = config ('db.comics');
+    //dd($comics);
+    return view('products', compact('comics'));
+})->name('products');
