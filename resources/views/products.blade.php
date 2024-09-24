59c3f62673724cac4e1ad4ccb35f1ce425e6c41f
AndyDAntuono <andrea.dantuono88@gmail.com>
2024-09-24 23:11:48 +0200

    [UPD] updated product.blade.php

--- resources/views/products.blade.php
+++ resources/views/products.blade.php
@@ -16,13 +16,6 @@
                     <img src="{{ $comic['thumb'] }}" class="card-img-top" alt="{{ $comic['title'] }}">
                     <div class="card-body">
                         <h5 class="card-title">{{ $comic['title'] }}</h5>
-                        <p class="card-text">{{ $comic['description'] }}</p>
-                        <ul class="list-group list-group-flush">
-                            <li class="list-group-item"><strong>Prezzo:</strong> {{ $comic['price'] }}</li>
-                            <li class="list-group-item"><strong>Serie:</strong> {{ $comic['series'] }}</li>
-                            <li class="list-group-item"><strong>Data di uscita:</strong> {{ $comic['sale_date'] }}</li>
-                            <li class="list-group-item"><strong>Tipo:</strong> {{ $comic['type'] }}</li>
-                        </ul>
                     </div>
                 </div>
             </div>
