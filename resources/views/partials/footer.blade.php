969c9317d767038cd7dce3aa2aeabaa97132de71
AndyDAntuono <andrea.dantuono88@gmail.com>
2024-09-24 20:42:09 +0200

    [UPD] added footer.blade.php; updated app.blade.php, header.blade.php, appp.scss files

---
+++ resources/views/partials/footer.blade.php
@@ -0,0 +1,92 @@
+<footer>
+    <div class="container">
+        <div class="row">
+            <div class="col-12">
+                <div class="d-flex justify-content-between align-items-center bg-primary text-white p-3">
+                    <span class="icon_and-text">
+                        <img src="../assets/img/buy-comics-digital-comics.png" alt="">
+                        <p>DIGITAL COMICS</p>
+                    </span>
+                    <span class="icon_and-text">
+                        <img src="../assets/img/buy-comics-merchandise.png" alt="">
+                        <p>DC MERCHANDISE</p>
+                    </span>
+                    <span class="icon_and-text">
+                        <img src="../assets/img/buy-comics-subscriptions.png" alt="">
+                        <p>SUBSCRIPTIONS</p>
+                    </span>
+                    <span class="icon_and-text">
+                        <img src="../assets/img/buy-comics-shop-locator.png" alt="">
+                        <p>COMIC SHOP LOCATOR</p>
+                    </span>
+                    <span class="icon_and-text">
+                        <img src="../assets/img/buy-dc-power-visa.svg" alt="">
+                        <p>DC POWER VISA</p>
+                    </span>
+                </div>
+            </div>
+        </div>
+        <div class="row bg-footer">
+            <div class="col-12">
+                <div class="d-flex flex-row mb-3 text-white">
+                    <div class="p-2">
+                        <ul class="list-unstyled">DC COMICS
+                            <li><a href="">Characters</a></li>
+                            <li><a href="">Comics</a></li>
+                            <li><a href="">Movies</a></li>
+                            <li><a href="">TV</a></li>
+                            <li><a href="">Games</a></li>
+                            <li><a href="">Videos</a></li>
+                            <li><a href="">News</a></li>
+                        </ul>
+                        <ul class="list-unstyled">Shop
+                            <li><a href="">Shop DC</a></li>
+                            <li><a href="">Shop DC Collectibles</a></li>
+                        </ul>
+                    </div>
+                    <div class="p-2">
+                        <ul class="list-unstyled">DC
+                            <li><a href="">Terms Of Use</a></li>
+                            <li><a href="">Privacy policy (New)</a></li>
+                            <li><a href="">Ad Choices</a></li>
+                            <li><a href="">Advertising</a></li>
+                            <li><a href="">Jobs</a></li>
+                            <li><a href="">Subscriptions</a></li>
+                            <li><a href="">Talent Workshops</a></li>
+                            <li><a href="">CPSC Certificates</a></li>
+                            <li><a href="">Ratings</a></li>
+                            <li><a href="">Shop Help</a></li>
+                            <li><a href="">Contac Us</a></li>
+                        </ul>
+                    </div>
+                    <div class="p-2">
+                        <ul class="list-unstyled">SITES
+                            <li><a href="">DC</a></li>
+                            <li><a href="">MAD Magazine</a></li>
+                            <li><a href="">DC Kids</a></li>
+                            <li><a href="">DC Universe</a></li>
+                            <li><a href="">DC Power Visa</a></li>
+                        </ul>
+                    </div>
+                  </div>
+            </div>
+        </div>
+        <div class="row bg-bottom-footer py-2">
+            <div class="col-12">
+                <div class="d-flex justify-content-between align-items-center">
+                    <div class="sign-up_button p-1">SIGN UP</div>
+                    <div class="d-flex justify-content-between align-items-center">
+                        <div class="follow-us me-2">
+                            FOLLOW US
+                        </div>
+                        <img class="footer-icon me-2" src="../assets/img/footer-facebook.png" alt="">
+                        <img class="footer-icon me-2" src="../assets/img/footer-twitter.png" alt="">
+                        <img class="footer-icon me-2" src="../assets/img/footer-youtube.png" alt="">
+                        <img class="footer-icon me-2" src="../assets/img/footer-pinterest.png" alt="">
+                        <img class="footer-icon me-2" src="../assets/img/footer-periscope.png" alt="">
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</footer>
--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -1,39 +1,45 @@
 <header>
-    <div class="container-fluid">
+    <div class="container">
         <div class="row">
             <div class="col-12">
-                <div class="text-center">
+                <div class="d-flex justify-content-between align-intems-center py-3 bg-white">
                     <img src="{{ Vite::asset('resources/img/dc-logo.png') }}" alt="dc-logo.png">
+                    <nav class="navbar navbar-expand-lg">
+                        <div class="container-fluid">
+                            <div class="collapse navbar-collapse">
+                                <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                                    <li class="nav-item me-3">
+                                        <a class="nav-link" href="#">CHARACTERS</a>
+                                      </li>
+                                    <li class="nav-item me-3">
+                                      <a class="nav-link active" aria-current="page" href="#">COMICS</a>
+                                    </li>
+                                    <li class="nav-item me-3">
+                                      <a class="nav-link" href="#">TV</a>
+                                    </li>
+                                    <li class="nav-item me-3">
+                                      <a class="nav-link" href="#">GAMES</a>
+                                    </li>
+                                    <li class="nav-item me-3">
+                                      <a class="nav-link">COLLECTIBLES</a>
+                                    </li>
+                                    <li class="nav-item me-3">
+                                        <a class="nav-link">COLLECTIBLES</a>
+                                    </li>
+                                    <li class="nav-item me-3">
+                                        <a class="nav-link">VIDEOS</a>
+                                    </li>
+                                    <li class="nav-item me-3">
+                                        <a class="nav-link">NEWS</a>
+                                      </li>
+                                    <li class="nav-item me-3">
+                                        <a class="nav-link">SHOP</a>
+                                    </li>
+                                  </ul>
+                            </div>
+                        </div>
+                    </nav>
                 </div>
-                <ul class="navbar-nav d-flex flex-row">
-                    <li class="nav-item me-3">
-                        <a class="nav-link" href="#">CHARACTERS</a>
-                      </li>
-                    <li class="nav-item me-3">
-                      <a class="nav-link active" aria-current="page" href="#">COMICS</a>
-                    </li>
-                    <li class="nav-item me-3">
-                      <a class="nav-link" href="#">TV</a>
-                    </li>
-                    <li class="nav-item me-3">
-                      <a class="nav-link" href="#">GAMES</a>
-                    </li>
-                    <li class="nav-item me-3">
-                      <a class="nav-link">COLLECTIBLES</a>
-                    </li>
-                    <li class="nav-item me-3">
-                        <a class="nav-link">COLLECTIBLES</a>
-                    </li>
-                    <li class="nav-item me-3">
-                        <a class="nav-link">VIDEOS</a>
-                    </li>
-                    <li class="nav-item me-3">
-                        <a class="nav-link">NEWS</a>
-                      </li>
-                    <li class="nav-item me-3">
-                        <a class="nav-link">SHOP</a>
-                    </li>
-                </ul>
             </div>
         </div>
     </div>
